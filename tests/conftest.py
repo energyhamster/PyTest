a4d00ca981e60589d868802430db774914553a37
Vlad <vla.opanasenko@gmail.com>
2022-07-26 14:03:22 +0300

    Added: parametrization in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,18 @@ from random import randrange
 def get_number():
     return randrange(1, 1000, 5)
 
+
 def _calculate(a, b):
-    return a + b
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    else:
+        return None
+
 
 @pytest.fixture
 def calculate():
     return _calculate
+
 
 @pytest.fixture
 def make_number():
@@ -20,4 +26,3 @@ def make_number():
     number = randrange(1, 1000, 5)
     yield number
     print(f"Number at home {number}")
-
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,8 +1,26 @@
+import pytest
+
 from src.base_classes.response import Response
 from src.schemas.user import User
 
 
+@pytest.mark.production
 def test_getting_users_list(get_users, calculate, make_number):
     Response(get_users).assert_status_code(200).validate(User)
     print(calculate(1, 1))
     print(make_number)
+
+@pytest.mark.skip('[ISSUE-23233] Issue with network connection')
+def test_another():
+    assert 1 == 1
+
+@pytest.mark.development
+@pytest.mark.parametrize("first_value, second_value, result", [
+    (1, 2, 3),
+    (-1, -2, -3),
+    (-1, 2, 1),
+    ("a", 2, None),
+    ("a", "b", None)
+])
+def test_calculator(first_value, second_value, result, calculate):
+    assert calculate(first_value, second_value) == result

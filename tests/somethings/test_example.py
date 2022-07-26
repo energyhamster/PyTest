0522eb4a62aebee010cb715bd30363f04d12db21
Vlad <vla.opanasenko@gmail.com>
2022-07-26 12:34:53 +0300

    Added: fixtures in conftest; Updated: test for user

---
+++ tests/somethings/test_example.py
@@ -0,0 +1,4 @@
+
+
+def test_get_number(get_number):
+    print(get_number)
---
+++ tests/users/test_users.py
@@ -0,0 +1,8 @@
+from src.base_classes.response import Response
+from src.schemas.user import User
+
+
+def test_getting_users_list(get_users, calculate, make_number):
+    Response(get_users).assert_status_code(200).validate(User)
+    print(calculate(1, 1))
+    print(make_number)
